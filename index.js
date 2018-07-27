02f12f290e0420f16a1f449946daaed251132fbc
Evan Solomon <evanlsolomon@gmail.com>
2018-07-27 14:54:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,9 @@ function nameToAttributes(parameter){
 
 function nameIsFromHometown(driver){
   debugger
+  console.log driver.name +' is from '+ driver.hometowm;
   return driver.name +' is from '+ driver.hometowm;
+
 }
 
 function attributesToPhrase(parameter){
