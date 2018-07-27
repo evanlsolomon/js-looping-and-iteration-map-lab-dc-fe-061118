1d351e04c398656b9ce2268bdd22de15448353a2
Evan Solomon <evanlsolomon@gmail.com>
2018-07-27 14:24:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file.
+const drivers = [];
+
+function lowerCaseNames(x) {
+return x.toLowerCase();
+}
+
+function lowerCaseDrivers(anything){
+
+  return anything.map(lowerCaseNames);
+}
+
+function splitNames(parameter){
+  return parameter.split(" ");
+}
+
+function nameToAttributes(anything){
+drivers.push('Bobby Smith', 'Sammy Watkins', 'Sally Jenkins', 'Annette Sawyer', 'Sarah Hucklebee', 'bobby ridge');
+  return anything.map(splitNames);
+}
+
+
