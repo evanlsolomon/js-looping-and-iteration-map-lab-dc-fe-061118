78728aab9bd587fa0f6583ca25288d068777cd27
Evan Solomon <evanlsolomon@gmail.com>
2018-07-27 14:36:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 // Code your solution in this file.
 const drivers = [];
 
+
 function lowerCaseNames(x) {
 return x.toLowerCase();
 }
-
 function lowerCaseDrivers(anything){
   return anything.map(lowerCaseNames);
 }
+
+
 
 function nameSplitter(name){
   firstName = name.split(" ")[0];
   lastName = name.split(" ")[1];
   return {firstName:firstName, lastName:lastName};
 }
-
 function nameToAttributes(parameter){
-
   return parameter.map(nameSplitter);
 }
 
